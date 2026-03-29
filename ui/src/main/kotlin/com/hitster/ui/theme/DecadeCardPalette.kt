b826fb1a1ac39e3371394f3e38e61966e9aa83b3
Vlad Dogadaev <dogadaev@users.noreply.github.com>
2026-03-29 22:08:44 +0200

    Tune timeline card palette to match lounge theme

--- ui/src/main/kotlin/com/hitster/ui/theme/DecadeCardPalette.kt
+++ ui/src/main/kotlin/com/hitster/ui/theme/DecadeCardPalette.kt
@@ -15,51 +15,51 @@ internal object DecadeCardPalettes {
         val decade = normalizeDecade(releaseYear)
         return when (decade) {
             1950 -> DecadeCardPalette(
-                topColor = 0xF8E0CCFF,
-                bottomColor = 0xD5A784FF,
-                edgeColor = 0xF8EAD7FF,
+                topColor = 0xF7DDC6FF,
+                bottomColor = 0xD39B79FF,
+                edgeColor = 0xFCEEDFFF,
             )
 
             1960 -> DecadeCardPalette(
-                topColor = 0xE8DB91FF,
-                bottomColor = 0xBC9348FF,
-                edgeColor = 0xF5E9B8FF,
+                topColor = 0xE8D18CFF,
+                bottomColor = 0xB18447FF,
+                edgeColor = 0xFBE9C6FF,
             )
 
             1970 -> DecadeCardPalette(
-                topColor = 0xF5C97DFF,
-                bottomColor = 0xD58D3DFF,
-                edgeColor = 0xFFE1A8FF,
+                topColor = 0xF1B96FFF,
+                bottomColor = 0xCE7E37FF,
+                edgeColor = 0xFDE0A7FF,
             )
 
             1980 -> DecadeCardPalette(
-                topColor = 0xF5A672FF,
-                bottomColor = 0xCD6D42FF,
-                edgeColor = 0xFFD0A7FF,
+                topColor = 0xEC8E62FF,
+                bottomColor = 0xC25833FF,
+                edgeColor = 0xF8C4A4FF,
             )
 
             1990 -> DecadeCardPalette(
-                topColor = 0xE68EA5FF,
-                bottomColor = 0xB75172FF,
-                edgeColor = 0xF4C0CBFF,
+                topColor = 0xD87D90FF,
+                bottomColor = 0xA54161FF,
+                edgeColor = 0xF0B8C5FF,
             )
 
             2000 -> DecadeCardPalette(
-                topColor = 0xD8A8F0FF,
-                bottomColor = 0x9E63CBFF,
-                edgeColor = 0xEACAF9FF,
+                topColor = 0xC58FE0FF,
+                bottomColor = 0x864CADFF,
+                edgeColor = 0xE7CDF7FF,
             )
 
             2010 -> DecadeCardPalette(
-                topColor = 0xAC9BECFF,
-                bottomColor = 0x735CC8FF,
-                edgeColor = 0xDDD2FFFF,
+                topColor = 0x8D74D5FF,
+                bottomColor = 0x5A3E98FF,
+                edgeColor = 0xDCCCFBFF,
             )
 
             else -> DecadeCardPalette(
-                topColor = 0x7FD8CAFF,
-                bottomColor = 0x38A094FF,
-                edgeColor = 0xCCF4EDFF,
+                topColor = 0x63BFA8FF,
+                bottomColor = 0x2E8275FF,
+                edgeColor = 0xC8EEE5FF,
             )
         }
     }
